4ef78fbf16ef95b4236712f365e0197cf81473c2
the-online-beast <strengthos.corp@gmail.com>
2026-06-16 09:09:55 -0600

    Create jobs.js

---
+++ jobs.js
@@ -0,0 +1,10 @@
+// ============================================================
+// CONFIGURATION — Google Sheet
+// Remplace SHEET_ID par l'ID de ton Google Sheet
+// L'ID se trouve dans l'URL :
+// https://docs.google.com/spreadsheets/d/ >>SHEET_ID<< /edit
+// ============================================================
+
+const SHEET_ID = '1vE-EIMhnGogl06y88CRh0Fu3mt6IGBWUthgPAeJHj8A';
+
+const SHEET_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:json&sheet=Sheet1`;
