06f7c9cc2e3c75fb274f5f9a4dee805557752e7b
the-online-beast <strengthos.corp@gmail.com>
2026-06-18 15:37:35 -0600

    Update jobs.js

--- jobs.js
+++ jobs.js
@@ -22,8 +22,8 @@
 
 const SHEET_VACANCIES_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRUhMgmCGpq1KM4-_Aig--5Vc1qdBctYkS3huaY_yw3hcs3q5oEJ7gKXsx1aIPCFtR13d7AguEdKeHN/pub?output=csv';
 
-const SHEET_CANDIDATES_URL = 'https://docs.google.com/spreadsheets/d/1ZPuAG5ymp1SD_GcKRias41eynwzdfS6MNdGV6BrNSHI/edit';
+const SHEET_CANDIDATES_URL = 'https://docs.google.com/spreadsheets/d/1ZPuAG5ymp1SD_GcKRias41eynwzdfS6MNdGV6BrNSHI/edit?gid=0#gid=0';
 
-const CV_WEBHOOK    = 'https://n8n.strength-os.tech/webhook/fc310eee-adce-486e-913b-8e5b6e48f24f';
+const CV_WEBHOOK    = 'https://n8n.strength-os.tech/webhook/cv-submit';
 const APPLY_WEBHOOK = 'https://n8n.strength-os.tech/webhook/apply';
 const DRIVE_FOLDER  = 'https://drive.google.com/drive/folders/15qYwk0SJC-K3pWNdoOV-bR7f7qNjacza';
