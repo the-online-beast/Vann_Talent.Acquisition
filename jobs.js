c9487eda9686650ba1043f46a44e00f64fe638f0
the-online-beast <strengthos.corp@gmail.com>
2026-06-21 17:09:09 -0600

    Remove empty lines from jobs.js

--- jobs.js
+++ jobs.js
@@ -2,5 +2,3 @@
 const SHEET_URL     = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyMUBGgcev-QNFR998-lUvC5bsAuAabM-IkPt2vlLCpjWTOAXVj3jhK_wV0COYcpkvu5OhFe8gJqfB/pub?gid=0&single=true&output=csv';
 const APPLY_WEBHOOK = 'https://n8n.strength-os.tech/webhook/apply';
 const DRIVE_FOLDER  = 'https://drive.google.com/drive/folders/1Kbw5R-BmeCH9QvbeewyZYnrcpjKU5C0b';
-
-
